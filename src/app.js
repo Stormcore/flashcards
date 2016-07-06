5acef274c30a479cb4ecec6152c4abeb8f4fc6e0
dzmitrysukhau <stormdjent@gmail.com>
2016-07-06 22:10:02 +0300

    sidebar add decks

--- src/app.js
+++ src/app.js
@@ -54,10 +54,15 @@ const store = Redux.createStore(Redux.combineReducers({
 }));
 
 const Sidebar = React.createClass({
+  componentDidUpdate() {
+    let element = ReactDOM.findDOMNode(this.refs.add);
+    if (element) element.focus();
+  },
   render() {
     let props = this.props;
     return(<div className='sidebar'>
       <h2>ALL DECKS</h2>
+      <button onClick={e => this.props.showAddDeck()}>New Deck</button>
       <ul>
         {
           props.decks.map((deck, i) =>
@@ -65,8 +70,14 @@ const Sidebar = React.createClass({
           )
         }
       </ul>
-      { props.addingDeck && <input ref='add' /> }
+      { props.addingDeck && <input ref='add' onKeyPress={this.createDeck} /> }
     </div>);
+  },
+  createDeck(event) {
+    if (event.which !== 13) return;
+    let name = ReactDOM.findDOMNode(this.refs.add).value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
   }
 });
 
@@ -79,8 +90,15 @@ const App = (props) => {
 function run() {
   let state = store.getState();
   console.log(state);
-  ReactDOM.render(<app>
-      <Sidebar decks={state.decks} addingDeck={state.addingDeck} />
+  ReactDOM.render(
+    <app>
+      <Sidebar
+        decks={state.decks}
+        addingDeck={state.addingDeck}
+        addDeck={name => store.dispatch(addDeck(name))}
+        showAddDeck={() => store.dispatch(showAddDeck())}
+        hideAddDeck={() => store.dispatch(hideAddDeck())}
+      />
     </app>,
      document.getElementById('root'));
 }
@@ -88,7 +106,3 @@ function run() {
 run();
 
 store.subscribe(run);
-
-window.show = () => store.dispatch(showAddDeck());
-window.hide = () => store.dispatch(hideAddDeck());
-window.add = () => store.dispatch(addDeck(new Date().toString()));
